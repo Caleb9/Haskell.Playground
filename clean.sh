fe98008f93f8b6d51e75a8739565e9766964566b
Piotr Karasinski <peter.karasinski@gmail.com>
2017-04-22 09:22:59 +0200

    Updated clean.sh to omit .git subdirectory.

--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@
 
 echo "# Removing following files:"
 
-find -type f -regextype posix-egrep -regex "\.(/\w+)+(\.(hi|o))?|\..+~" -delete -print
+find -type f -regextype posix-egrep \
+     \( ! -regex "\./\.git/.*" -and -regex "\.(/\w+)+(\.(hi|o))?|\..+~" \) \
+     -delete -print
 
 echo "# Done"
